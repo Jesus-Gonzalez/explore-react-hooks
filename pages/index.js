fda77c019e40807e49e4dcee492e501b83d5f847
Jesús González Jaén <jesus.gonzalez.development@gmail.com>
2021-03-20 12:44:39 +0100

    feature(2/useMemo): simple useMemo usage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import { useMemo, useState } from "react"
+
 export default function Home() {
+  const [value, setValue] = useState('');
+
+  const uppercasedValue = useMemo(() => value.toUpperCase(), [value]); // expensive computation
+
+  const handleChangeInput = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div>
+      <input onChange={handleChangeInput} />
+      <div>
+        <span>Uppercased:</span>
+        <strong>{uppercasedValue}</strong>
+      </div>
     </div>
   )
 }
